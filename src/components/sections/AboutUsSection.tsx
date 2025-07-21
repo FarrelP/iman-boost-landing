e6363e3adb5b08379902369915640420670443e6
Farrel Priambodo <farrelandroid99@gmail.com>
2025-07-21 17:31:45 +0700

    Update section About Us and Product

--- src/components/sections/AboutUsSection.tsx
+++ src/components/sections/AboutUsSection.tsx
@@ -1,5 +1,5 @@
-// src/components/AboutUsSection.tsx (Versi yang Disesuaikan)
 import Image from 'next/image';
+import Card from '../common/Card';
 
 export default function AboutUsSection() {
   const aboutText = `
@@ -9,40 +9,34 @@ export default function AboutUsSection() {
   `;
 
   return (
-    <section className="bg-white py-16 px-4 sm:px-6 lg:px-8"> {/* Hapus 'relative' di sini */}
-      <div className="container mx-auto max-w-6xl"> {/* Hapus 'relative z-10' di sini */}
-        {/* Main Card */}
-        <div className="bg-gray-100 rounded-lg shadow-xl pt-6 pl-6 pr-6 sm:pt-8 sm:pl-8 sm:pr-8 lg:pt-10 lg:pl-10 lg:pr-10 pb-0">
-          {/* Title Card */}
-          <div className="w-fit mx-auto -mt-16 mb-8 px-6 py-3 bg-gradient-to-r from-[#164d2e] to-[#006737] text-white text-center font-semibold rounded-lg shadow-md">
-            <h2 className="text-xl sm:text-2xl whitespace-nowrap">Tentang Iman Boost</h2>
-          </div>
-
-          {/* Content Division (Left: Text, Right: Image) */}
+    <section className="bg-white py-16 px-4 sm:px-6 lg:px-8">
+      <div className="container mx-auto max-w-6xl">
+        <Card 
+          className="pt-6 pl-6 pr-6 sm:pt-8 sm:pl-8 sm:pr-8 lg:pt-10 lg:pl-10 lg:pr-10 pb-0 rounded-lg" 
+          titleString="Tentang Iman Boost"
+          variant="primary">
           <div className="flex flex-col md:flex-row items-center gap-8">
-            {/* Left Section: Text Explanation */}
             <div className="md:w-1/2 text-gray-700 text-center sm:text-lg leading-relaxed">
               <p className="mb-4 font-semibold text-green-950">
-                {aboutText.split('\n\n')[0]} {/* Paragraf pertama */}
+                {aboutText.split('\n\n')[0]}
               </p>
               <p className="text-green-900">
-                {aboutText.split('\n\n')[1]} {/* Paragraf kedua */}
+                {aboutText.split('\n\n')[1]}
               </p>
             </div>
 
-            {/* Right Section: Image */}
             <div className="md:w-1/2 flex justify-center items-center">
               <Image
                 src="/images/temp_placeholder.webp"
                 alt="Tentang Produk"
-                width={300} // Sesuaikan lebar gambar
-                height={300} // Sesuaikan tinggi gambar
+                width={300}
+                height={300}
                 className="shadow-lg object-cover w-[23rem] h-auto max-h-80 md:max-h-96"
                 style={{ borderRadius: '50% 50% 0 0', border: '1rem #c8bebe solid', borderBottom: 'none' }}
               />
             </div>
           </div>
-        </div>
+        </Card>
       </div>
     </section>
   );
--- src/components/sections/ProductSection.tsx
+++ src/components/sections/ProductSection.tsx
@@ -1,56 +1,63 @@
-// src/components/ProductSection.tsx
 import Image from 'next/image';
+import Card from '../common/Card';
 
 export default function ProductSection() {
-  const shortText = `
-    Jelajahi beragam fitur dan kapabilitas unggulan dari produk kami yang dirancang untuk memenuhi setiap kebutuhan Anda. Kami menghadirkan inovasi terbaru untuk meningkatkan efisiensi dan kreativitas dalam setiap aspek pekerjaan Anda.
+  const productText = `
+    Serangkaian audio harian dan panduan kebiasaan yang membantu pengguna menjemput rezeki dengan cara spiritual, syar'i dan penuh makna.
+
+    di dalam Iman Boost adalah bagian khusus dari konten audio dan modul spiritual yang dirancang untuk membantu Muslim membangun mindset berbasi ajaran Islam, bukan sekedar afirmasi kosong.
   `;
 
+  const illustrationCardArr = [
+    {
+      title: "Magnet Rezeki",
+      imageSrc: "/images/temp_placeholder.webp",
+      altText: "Ilustrasi A Produk",
+    },
+    {
+      title: "Anxiety",
+      imageSrc: "/images/temp_placeholder.webp",
+      altText: "Ilustrasi B Produk",
+    },
+  ];
+  const borderCardName = "relative bg-white rounded-3xl shadow-xl flex flex-col items-center w-full max-w-sm sm:w-1/2";
+  const borderImageStyle = { border: ".6rem #c8bebe solid" };
+
   return (
-    <section className="bg-green-600 py-16 px-4 sm:px-6 lg:px-8 text-white">
+    <section className="bg-green-950 pt-14 pb-10 px-4 sm:px-6 lg:px-8 text-white">
       <div className="container mx-auto max-w-6xl">
         <div className="flex flex-col md:flex-row items-center md:items-start gap-12">
           {/* Left Section: Short Text */}
           <div className="md:w-1/2 text-center md:text-left">
-            <h2 className="text-4xl sm:text-5xl font-extrabold mb-6">Fitur Unggulan Produk Kami</h2>
-            <p className="text-lg sm:text-xl leading-relaxed">
-              {shortText}
+            <h2 className="text-4xl sm:text-5xl font-extrabold mb-6">Produk Audio</h2>
+            <p className="sm:text-xl leading-relaxed font-thin">
+              <span className="font-semibold">Magnet Rezeki</span> {productText.split('\n\n')[0]}
+            </p>
+            <br />
+            <p className="sm:text-xl leading-relaxed font-thin">
+              <span className="font-semibold">Anxiety</span> {productText.split('\n\n')[1]}
             </p>
           </div>
 
           {/* Right Section: Two Illustrations */}
           <div className="md:w-1/2 flex flex-col sm:flex-row gap-8 justify-center">
-            {/* Illustration A Card */}
-            <div className="relative bg-white rounded-3xl shadow-xl p-4 flex flex-col items-center w-full max-w-sm sm:w-1/2">
-              {/* Title Card for Illustration A */}
-              <div className="absolute -top-6 px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md">
-                <h3 className="text-lg whitespace-nowrap">Ilustrasi A</h3>
-              </div>
-              <Image
-                src="/images/product-ilustrasi-a.jpg" // Ganti dengan path gambar Ilustrasi A
-                alt="Ilustrasi A Produk"
-                width={300} // Sesuaikan lebar gambar
-                height={200} // Sesuaikan tinggi gambar
-                className="mt-8 rounded-2xl object-cover w-full h-auto"
-              />
-              <p className="text-gray-800 text-center mt-4">Deskripsi singkat Ilustrasi A.</p>
-            </div>
-
-            {/* Illustration B Card */}
-            <div className="relative bg-white rounded-3xl shadow-xl p-4 flex flex-col items-center w-full max-w-sm sm:w-1/2">
-              {/* Title Card for Illustration B */}
-              <div className="absolute -top-6 px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md">
-                <h3 className="text-lg whitespace-nowrap">Ilustrasi B</h3>
-              </div>
-              <Image
-                src="/images/product-ilustrasi-b.jpg" // Ganti dengan path gambar Ilustrasi B
-                alt="Ilustrasi B Produk"
-                width={300} // Sesuaikan lebar gambar
-                height={200} // Sesuaikan tinggi gambar
-                className="mt-8 rounded-2xl object-cover w-full h-auto"
-              />
-              <p className="text-gray-800 text-center mt-4">Deskripsi singkat Ilustrasi B.</p>
-            </div>
+            {illustrationCardArr.map((item) => (
+              <Card 
+                key={item.title}
+                className={borderCardName} 
+                titleString={item.title} 
+                variant="secondary"
+              >
+                <Image
+                  src={item.imageSrc}
+                  alt={item.altText}
+                  width={300}
+                  height={200}
+                  className="rounded-3xl object-cover w-full h-full"
+                  style={borderImageStyle}
+                />
+              </Card>
+            ))}
           </div>
         </div>
       </div>
