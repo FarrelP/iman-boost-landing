993545bc63ecf76dcad92335ca6c3c75d2c73ae0
Farrel Priambodo <farrelandroid99@gmail.com>
2025-07-29 07:02:09 +0700

    Update deprecated export

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,13 @@ const isGithubPages = process.env.GITHUB_PAGES === 'true';
 const repo = 'iman-boost_landing';
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '',
+  reactStrictMode: true,
   images: {
     domains: ['placehold.co'],
   },
-  /* config options here */
 };
 
 export default nextConfig;
