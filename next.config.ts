7e5b1a7542f44d282a29aa758546c25007ff7af8
Farrel Priambodo <farrelandroid99@gmail.com>
2025-07-29 08:30:03 +0700

    Fix deploy issue - missing css and js

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,9 @@ const nextConfig: NextConfig = {
   output: 'export',
   basePath: isGithubPages ? `/${repo}` : '',
   assetPrefix: isGithubPages ? `/${repo}/` : '',
+  trailingSlash: true,
   reactStrictMode: true,
-  images: {
-    domains: ['placehold.co'],
-  },
+  images: { domains: ['placehold.co'] },
 };
 
 export default nextConfig;
